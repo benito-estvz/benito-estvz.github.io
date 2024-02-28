f18cb3e2784223252b11f6eb103c4278aa76d5c7
benito-estvz <158457454+benito-estvz@users.noreply.github.com>
2024-02-28 10:47:16 +0000

    Añadido botón para ver información de seguridad Añadido punto dos: "Privacidad digital" Eliminado botón 3 main.js v1.1

--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,12 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   });
+  document.addEventListener('DOMContentLoaded', function() {
+    const fontCodeButtons = document.querySelectorAll('.btnvirustotalreport');
+  
+    fontCodeButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        window.open('https://www.virustotal.com/gui/url/a47c8134abd15cac2c54bed509f7e787cae59962badd7da71e0ba98a0f6724b4?nocache=1', '_blank');
+      });
+    });
+  });

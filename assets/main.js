822a318d1c02c3cd3db19ef757f2048eecc63317
benito-estvz <158457454+benito-estvz@users.noreply.github.com>
2024-03-07 09:46:37 +0000

    Añadida nueva sección y muchos botones main.js v1.2

--- assets/main.js
+++ assets/main.js
@@ -16,3 +16,93 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const fontCodeButtons = document.querySelectorAll('.btnairgeddon');
+  
+    fontCodeButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        window.open('https://github.com/v1s1t0r1sh3r3/airgeddon', '_blank');
+      });
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const fontCodeButtons = document.querySelectorAll('.btnparrot');
+  
+    fontCodeButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        window.open('https://parrot.sh', '_blank');
+      });
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const fontCodeButtons = document.querySelectorAll('.btnxfw');
+  
+    fontCodeButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        window.open('https://flipper-xtre.me', '_blank');
+      });
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const fontCodeButtons = document.querySelectorAll('.btnmayhem');
+  
+    fontCodeButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        window.open('https://github.com/portapack-mayhem/mayhem-firmware', '_blank');
+      });
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const fontCodeButtons = document.querySelectorAll('.btnhak5');
+  
+    fontCodeButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        window.open('https://hak5.org', '_blank');
+      });
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const fontCodeButtons = document.querySelectorAll('.btnelladodelmal');
+  
+    fontCodeButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        window.open('https://www.elladodelmal.com/', '_blank');
+      });
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const fontCodeButtons = document.querySelectorAll('.btnflipper');
+  
+    fontCodeButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        window.open('https://flipperzero.one/', '_blank');
+      });
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const fontCodeButtons = document.querySelectorAll('.btngsg');
+  
+    fontCodeButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        window.open('https://greatscottgadgets.com/', '_blank');
+      });
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const fontCodeButtons = document.querySelectorAll('.btnds');
+  
+    fontCodeButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        window.open('https://dstike.org/', '_blank');
+      });
+    });
+  });
